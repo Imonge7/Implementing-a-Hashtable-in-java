90b69c052fdd67d17680b972fadb1a9c22cb004f
Imonge7 <150256892+Imonge7@users.noreply.github.com>
2023-11-15 15:07:02 +0200

    Main1.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class Main {
+public class Main { 
 
     public static void main(String[] args) {
 	     // key: Employee Number (Integer)
